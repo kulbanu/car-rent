b30bcc330c9777e99deab02706394f8e879f9db1
Kulbanu <r.r.r.r.4.g.a@gmail.com>
2024-02-11 02:48:24 +0600

    fix: footer

--- src/templates/MainPageTemplate.tsx
+++ src/templates/MainPageTemplate.tsx
@@ -4,12 +4,12 @@ import { ScrollToTop } from '../utils/scrollToTop'
 
 export const MainPageTemplate = () => {
   return (
-    <div className="min-h-screen relative flex flex-col overflow-x-hidden">
+    <div className="min-h-screen relative flex flex-col justify-between overflow-x-hidden">
       <NavBar />
-      <div className="mt-16 md:mt-0">
+      <div className="mb-auto mt-16 md:mt-0">
         <Outlet />
       </div>
-      <Footer className=" self-end" />
+      <Footer className="self-end" />
       <Modal />
       <ScrollToTop />
     </div>
